56dcacc75681edc54b8877323bbadf70169b41ea
Alexis Tran <alexis.tran@thalesdigital.io>
2021-06-10 15:14:07 +0800

    fix: rework on the algo and add comment

--- http/handlers/get_shortest_path.go
+++ http/handlers/get_shortest_path.go
@@ -24,21 +24,21 @@ func computePath(src string, dest string) ([][]string, error) {
 	queue := [][]string{{src}}
 
 	for len(queue) > 0 {
-		// Select first element in the queue
+		// Select first element in the queue, equivalent of a pop operation
 		path := queue[0]
-
-		// Shift element of the queue, equivalent of a .pop operation
 		queue = queue[1:]
 
-		// Select last node in the path
+		// Select last station in the path
 		lastNode := path[len(path)-1]
 		visited[lastNode] = true
 
 		// Check if the dest is reached
 		if lastNode == dest {
 			res = append(res, path)
+			// Reset the visited value for future possible paths to be discovered
 			visited[lastNode] = false
 		} else {
+			// Add the next stations to the possible paths and append those path to the queue
 			for _, stn := range graph[lastNode] {
 				if !visited[stn] {
 					temp := make([]string, len(path))
@@ -46,13 +46,14 @@ func computePath(src string, dest string) ([][]string, error) {
 					path := append(temp, stn)
 					queue = append(queue, path)
 
+					// Make sure that we can't revisit an already visited station
 					visited[stn] = true
 				}
 			}
 		}
 	}
 
-	fmt.Print("Final res", res)
+	fmt.Print("Paths", res)
 
 	return res, nil
 }
